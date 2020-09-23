d994537c92a28ea916596445547636eba5a9c544
nessx <cf17nestor.santana@iesjoandasutria.org>
2020-09-23 20:22:48 +0200

    holis

--- app/src/main/java/com/example/gestor_incidencias/MainActivity.java
+++ app/src/main/java/com/example/gestor_incidencias/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.gestor_incidencias;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.google.android.material.snackbar.Snackbar;
+
 
 public class MainActivity extends AppCompatActivity {
+    //declaracion de variables junto a su tipo
+    Button logbtn;
+    EditText loguser, logpass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //De declaracion de variables
+        loguser = findViewById(R.id.loguser);
+        logpass = findViewById(R.id.logpass);
+        logbtn = findViewById(R.id.logbtn);
+
+        //codigo del botton
+        logbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ((loguser.getText().toString().trim().equals("admin")) && (logpass.getText().toString().trim().equals("1234"))){
+                    Intent intent = new Intent (MainActivity.this, iniciomenu.class);
+                    startActivity(intent);
+                }
+                else{
+                    Snackbar.make(v, "Usuario o contraseña incorrecto!", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/gestor_incidencias/iniciomenu.java
@@ -0,0 +1,18 @@
+package com.example.gestor_incidencias;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class iniciomenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_inicio);
+
+        //codigo
+
+    }
+}
