e0ef41238ba7553bc96771f77b0fc914174c7e89
nessx <cf17nestor.santana@iesjoandasutria.org>
2020-09-25 19:44:14 +0200

    avanzado

--- app/src/main/java/com/example/gestor_incidencias/clases/agregar.java
+++ app/src/main/java/com/example/gestor_incidencias/clases/agregar.java
@@ -1,4 +1,26 @@
 package com.example.gestor_incidencias.clases;
 
-public class agregar {
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.gestor_incidencias.*;
+
+public class agregar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.agregar);
+
+        //codigo
+        Spinner dropdown = findViewById(R.id.urgencia);
+        String[] items = new String[]{"1", "2", "three"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
+        // fragments pasar datos tutorals
+
+    }
 }
--- app/src/main/java/com/example/gestor_incidencias/iniciomenu.java
+++ app/src/main/java/com/example/gestor_incidencias/iniciomenu.java
@@ -1,18 +1,35 @@
 package com.example.gestor_incidencias;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.gestor_incidencias.clases.*;
 
 
 
 public class iniciomenu extends AppCompatActivity {
+    Button agregar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_inicio);
+        agregar = findViewById(R.id.agregar);
+
 
         //codigo
+
+        agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (iniciomenu.this, agregar.class);
+                startActivity(intent);
+            }
+        });
 
         // fragments pasar datos tutorals
 
