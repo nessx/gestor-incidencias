e0bae1f73d583ccce61abdf7b7de87e9d901c90b
nessx <cf17nestor.santana@iesjoandaustria.org>
2020-11-22 01:51:45 +0100

    se ha implementado un nuevo menu <3

--- app/src/main/java/com/example/gestor_incidencias/clases/ComunicaMenu.java
+++
@@ -1,6 +0,0 @@
-package com.example.gestor_incidencias.clases;
-
-public interface ComunicaMenu {
-
-    public void menu(int btnclick);
-}
--- app/src/main/java/com/example/gestor_incidencias/iniciomenu.java
+++ app/src/main/java/com/example/gestor_incidencias/iniciomenu.java
@@ -1,41 +1,34 @@
 package com.example.gestor_incidencias;
 
 import android.content.DialogInterface;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.RectF;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.gestor_incidencias.clases.RecyclerViewAdapter;
 import com.example.gestor_incidencias.clases.agregar;
-import com.example.gestor_incidencias.clases.incidencia;
-import com.example.gestor_incidencias.clases.usuario;
+import com.example.gestor_incidencias.clases.listar;
 import com.example.gestor_incidencias.db.IncidenciaDBHelper;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.google.android.material.navigation.NavigationView;
 
 
-public class iniciomenu extends AppCompatActivity {
+public class iniciomenu extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,
+        DrawerLayout.DrawerListener {
     private RecyclerViewAdapter adapter;
     private RecyclerView recyclerView;
     private AlertDialog.Builder alertDialog;
@@ -47,8 +40,9 @@ public class iniciomenu extends AppCompatActivity {
 
     private IncidenciaDBHelper dbHelper;
     private SQLiteDatabase db;
+    private DrawerLayout drawerLayout;
+    private Fragment fragment;
 
-    TextView userview;
     protected Fragment[] menuFragments;
 
     @Override
@@ -60,9 +54,123 @@ public class iniciomenu extends AppCompatActivity {
         dbHelper = new IncidenciaDBHelper(getApplicationContext());
         db = dbHelper.getWritableDatabase();
 
-        usuario user = new usuario();
-        userview = findViewById(R.id.userview);
-        userview.setText(getResources().getString(R.string.dialog_welcome)+" "+user.getuser().toUpperCase());
+        //usuario user = new usuario();
+        //TextView userview = findViewById(R.id.userview);
+        //userview.setText(getResources().getString(R.string.dialog_welcome)+" "+user.getuser().toUpperCase());
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.fragmentID);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        MenuItem menuItem = navigationView.getMenu().getItem(0);
+        onNavigationItemSelected(menuItem);
+        menuItem.setChecked(true);
+
+        drawerLayout.addDrawerListener(this);
+
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int title;
+        switch (menuItem.getItemId()) {
+            case R.id.afegir:
+                fragment = new agregar();
+                break;
+            case R.id.llistar:
+                fragment = new listar();
+                break;
+            case R.id.eliminar:
+                dbHelper.delete();
+                break;
+            case R.id.nav_share:
+                title = R.string.menu_share;
+                break;
+            case R.id.nav_send:
+                title = R.string.menu_send;
+                break;
+            default:
+                throw new IllegalArgumentException("menu option not implemented!!");
+        }
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setCustomAnimations(R.anim.bottom_nav_enter, R.anim.bottom_nav_exit)
+                .replace(R.id.home_content, fragment)
+                .commit();
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    @Override
+    public void onDrawerSlide(@NonNull View view, float v) {
+        //cambio en la posición del drawer
+    }
+
+    @Override
+    public void onDrawerOpened(@NonNull View view) {
+        //el drawer se ha abierto completamente
+    }
+
+    @Override
+    public void onDrawerClosed(@NonNull View view) {
+        //el drawer se ha cerrado completamente
+    }
+
+    @Override
+    public void onDrawerStateChanged(int i) {
+        //cambio de estado, puede ser STATE_IDLE, STATE_DRAGGING or STATE_SETTLING
+    }
+
+    public void confirm() {
+        //AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getApplicationContext());
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getApplicationContext(), R.style.Theme_AppCompat_Light);
+
+
+        // set title
+        alertDialogBuilder.setTitle(R.string.dialog_cln_tl);
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage(R.string.dialog_mensaje)
+                .setCancelable(false)
+                .setPositiveButton(R.string.dialog_aceptar,new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        dbHelper.delete();
+                        Toast.makeText(getApplicationContext(), getString(R.string.dialog_del_msg),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton(R.string.dialog_cancelar,new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        Toast.makeText(getApplicationContext(), getString(R.string.dialog_cln_msg),
+                                Toast.LENGTH_SHORT).show();
+                        dialog.cancel();
+                    }
+                });
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        // show it
+        alertDialog.show();
     }
 
 }
